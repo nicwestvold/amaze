dc17ad12870e791acba97e5b6ae7b41568d3d003
Nic Westvold <nicwestvold@gmail.com>
2023-03-08 18:21:51 -0500

    updates getPrimes

--- src/getPrimes.js
+++ src/getPrimes.js
@@ -1,17 +1,74 @@
+/**
+ * Get the first `n` prime numbers
+ */
 export const getPrimes = (count) => {
 	const primes = [];
 	if (!count) return primes;
-	const n = 100;
-	const sieve = new Array(n + 1).fill(true);
 
-	for (let i = 2; i <= n; i++) {
-		if (sieve[i]) {
-			primes.push(i);
-			for (let j = i * i; j <= n; j += i) {
-				sieve[j] = false;
-			}
+	let start = 2;
+	let stop = findN(count);
+	const sieve = populateSieve(start, stop);
+
+	let i = 0;
+
+	while (count > primes.length && i < 4) {
+		sieveOfEratosthenes(start, stop, count, sieve, primes);
+		if (count > primes.length) {
+			const diff = stop - start;
+			start = stop + 1;
+			stop = start + diff;
+			populateSieve(start, stop, sieve);
+			markSieve(stop, sieve, primes);
 		}
+		i++;
 	}
 
 	return primes.slice(0, count);
 };
+
+function sieveOfEratosthenes(start, stop, count, sieve, primes) {
+	for (let i = start; i <= stop; i++) {
+		if (sieve[i]) {
+			primes.push(i);
+			if (primes.length === count) return primes;
+			for (let j = i * i; j <= stop; j += i) {
+				sieve[j] = false;
+			}
+		}
+		// clean up
+		delete sieve[i];
+	}
+	return primes;
+}
+
+/**
+ * Encapsulates the logic of finding an appropriate `n`
+ */
+function findN(count) {
+	return Math.ceil(count * Math.log(count));
+}
+
+/**
+ * use an object as the sieve
+ */
+function populateSieve(start, stop, sieve = { length: 0 }) {
+	sieve.length += stop + 1;
+	for (let i = start; i <= stop; i++) {
+		sieve[i] = true;
+	}
+	return sieve;
+}
+
+/**
+ * marks a sieve's values as false, given an array of primes.
+ */
+function markSieve(stop, sieve, primes) {
+	for (let i = 0; i < primes.length; i++) {
+		const p = primes[i];
+		for (let j = p * p; j <= stop; j += p) {
+			if (sieve[j]) {
+				sieve[j] = false;
+			}
+		}
+	}
+}
